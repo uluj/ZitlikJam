bb1789858d3a5525cab1abca67fce1af83982f3b
Muhammet Emin Şen <m.emin.sen8@gmail.com>
2024-12-01 05:20:43 +0300

    ..

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] private float health = 100;
+    [SerializeField] private Image mainHealthBar;
+    [SerializeField] private Image backgroundHealthBar;
+    [SerializeField] private GameObject parentHealthBar;
+    [SerializeField] private float healthDeceleration = 0.5f;
+    [SerializeField] private GameObject diePanel;
+    private bool _isDamage;
+
+
+    public void TakeDamage(float damage)
+    {
+        Debug.Log("Player Health TakeDamage");
+        health -= damage;
+    }
+
+    private void Update()
+    {
+        backgroundHealthBar.fillAmount = Mathf.Lerp(backgroundHealthBar.fillAmount, health / 100f,
+            healthDeceleration * Time.deltaTime);
+        mainHealthBar.fillAmount = health / 100f;
+
+        if (Mathf.Approximately(backgroundHealthBar.fillAmount, 0))
+        {
+            diePanel.SetActive(true);
+        }
+    }
+}
